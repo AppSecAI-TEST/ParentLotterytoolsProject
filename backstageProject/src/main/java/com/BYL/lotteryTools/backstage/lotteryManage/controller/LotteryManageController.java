3eb245419aaf78b89bd8b4b7f5190d8ec4f9e956
banna <379334810@qq.com>
2017-03-13 08:43:22 +0800

    commit

---
+++ backstageProject/src/main/java/com/BYL/lotteryTools/backstage/lotteryManage/controller/LotteryManageController.java
@@ -0,0 +1,17 @@
+package com.BYL.lotteryTools.backstage.lotteryManage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 彩种维护controller
+* @Description: TODO(这里用一句话描述这个方法的作用) 
+* @author banna
+* @date 2017年3月3日 下午1:59:35
+ */
+@Controller
+@RequestMapping("/lotteryManage")
+public class LotteryManageController 
+{
+	
+}
