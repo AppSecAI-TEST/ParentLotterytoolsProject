fd861bd004c33d39fed2b48f0c7a4706672b8a94
banna <379334810@qq.com>
2017-07-22 18:07:32 +0800

    h5接口添加获取应用版本的接口

--- backstageProject/src/main/java/com/BYL/lotteryTools/backstage/lotterybuyerOfexpert/controller/OuterH5LotteryBuyerOrExpertController.java
+++ backstageProject/src/main/java/com/BYL/lotteryTools/backstage/lotterybuyerOfexpert/controller/OuterH5LotteryBuyerOrExpertController.java
@@ -15,13 +15,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.BYL.lotteryTools.backstage.app.controller.AppController;
+import com.BYL.lotteryTools.backstage.app.dto.AppversionDTO;
+import com.BYL.lotteryTools.backstage.app.entity.App;
+import com.BYL.lotteryTools.backstage.app.entity.Appversion;
+import com.BYL.lotteryTools.backstage.app.service.AppService;
+import com.BYL.lotteryTools.backstage.app.service.AppversionService;
 import com.BYL.lotteryTools.backstage.lotterybuyerOfexpert.dto.LotterybuyerOrExpertDTO;
 import com.BYL.lotteryTools.backstage.lotterybuyerOfexpert.service.LotterybuyerOrExpertService;
 import com.BYL.lotteryTools.backstage.outer.service.RongyunImService;
 import com.BYL.lotteryTools.common.bean.ResultBean;
+import com.BYL.lotteryTools.common.entity.Uploadfile;
 import com.BYL.lotteryTools.common.exception.GlobalOuterExceptionHandler;
 import com.BYL.lotteryTools.common.service.UploadfileService;
+import com.BYL.lotteryTools.common.util.Constants;
 
 /**
  * 
@@ -42,11 +51,18 @@ public class OuterH5LotteryBuyerOrExpertController extends GlobalOuterExceptionH
 	private RongyunImService rongyunImService;//融云service层
 	
 	@Autowired
+	private AppService appService;
+	
+	@Autowired
+	private AppversionService appversionService;
+	
+	@Autowired
 	private UploadfileService uploadfileService;
 	
 	public static Map<String,String> sessionMap = new HashMap<String, String>();
 	
 	public static final String DOMAIN="http://36.7.190.20:1881/";
+	
 	
 	public static String morenTouxiang = "0";//默认头像newsUuid
 	
@@ -137,6 +153,59 @@ public class OuterH5LotteryBuyerOrExpertController extends GlobalOuterExceptionH
 		response.setCharacterEncoding("utf-8");
 		response.setHeader("Access-Control-Allow-Origin", "*"); 
 		out.write(callback+"([ { resultCode:\""+result.get("resultCode")+"\",message:\""+result.get("message")+"\"}])");
+		out.flush();
+		out.close();
+	}
+	
+	@RequestMapping(value = "/getAppVersionOfApp", method = RequestMethod.GET)
+	public void getAppVersionOfApp(
+			@RequestParam(value="appName",required=false)String appName,
+			HttpServletRequest request,HttpSession httpSession,HttpServletResponse response) throws Exception
+	{
+		AppversionDTO dto = new AppversionDTO();
+		 PrintWriter out = response.getWriter();
+		 String callback = request.getParameter("jsoncallback");
+		response.setCharacterEncoding("utf-8");
+		response.setHeader("Access-Control-Allow-Origin", "*"); 
+		if(null != appName && !"".equals(appName))
+		{
+			App app = appService.getAppByAppName(appName);
+			if(null != app)
+			{
+				//获取当前appId下的且已上架的应用版本的最新版本数据
+				Integer maxVersionFlowId =
+						appversionService.
+						findMaxVersionFlowId(app.getId(), AppController.APP_V_STATUS_SJ);//获取当前应用下的应用版本数据是上架状态的最大版本流水号
+				if(null!=maxVersionFlowId)
+				{
+					Appversion appversion = appversionService.
+							getAppversionByAppIdAndVersionFlowId(app.getId(), maxVersionFlowId);//根据appId和版本流水号获取应用版本数据
+					
+					Uploadfile uploadfile = uploadfileService.getUploadfileByNewsUuid(appversion.getAppVersionUrl());
+					
+					String apkUrl = "";
+					if(null!=uploadfile)
+					{
+						apkUrl = uploadfile.getUploadfilepath()+uploadfile.getUploadRealName();//抓取附件的真实存储路径
+						appversion.setAppVersionUrl(apkUrl);
+					}
+					dto = appversionService.toDTO(appversion);
+					out.write(callback+"([ { resultCode:\""+Constants.SUCCESS_CODE+"\",apkUrl:\""+apkUrl+"\"}])");
+					
+				}
+				else
+				{
+					out.write(callback+"([ { resultCode:\""+Constants.FAIL_CODE_OF_NO_NEW_APPVERSION+"\",message:\""+"获取失败,当前应用没有最新版本"+"\"}])");
+				}
+			
+			}
+			else
+			{
+				LOG.error("getAppversionsOfStationCouldUse：当前应用的已发布的应用版本的版本好是null;应用name="+appName);
+				out.write(callback+"([ { resultCode:\""+Constants.FAIL_CODE_OF_NO_APP+"\",message:\""+"获取失败,应用名为所传参数的应用不存在"+"\"}])");
+			}
+			
+		}
 		out.flush();
 		out.close();
 	}
